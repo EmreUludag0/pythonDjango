e3b3bfb2f12190d1c6b8d57a4edb0e06f11d629d
EmreUludag0 <155458894+EmreUludag0@users.noreply.github.com>
2024-02-21 20:53:30 +0300

    2024

--- proje1/config/settings.py
+++ proje1/config/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates" # core klasörünün olduğu dizin. birlestirme yapılacak kod
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =[
+    BASE_DIR / "staticfiles"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
